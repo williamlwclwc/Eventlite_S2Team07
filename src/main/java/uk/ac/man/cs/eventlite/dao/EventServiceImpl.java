89f9f1b6c8be0e59e3daf37ed55db2d849440d9b
Jack Zhou <yuxuan.zhou@student.manchester.ac.uk>
2019-02-14 12:52:01 +0000

    Implemented save method

--- src/main/java/uk/ac/man/cs/eventlite/dao/EventServiceImpl.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/EventServiceImpl.java
@@ -1,17 +1,9 @@
 package uk.ac.man.cs.eventlite.dao;
 
-import java.util.Iterator;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import uk.ac.man.cs.eventlite.entities.Event;
 
@@ -25,32 +17,15 @@ public class EventServiceImpl implements EventService {
 
 	private final static String DATA = "data/events.json";
 
-	@Override
 	public long count() {
-		long count = 0;
-		Iterator<Event> i = findAll().iterator();
-
-		for (; i.hasNext(); count++) {
-			i.next();
-		}
-
-		return count;
+		return eventRepository.count();
 	}
 
-	@Override
 	public Iterable<Event> findAll() {
-		ArrayList<Event> events = new ArrayList<Event>();
-
-		try {
-			ObjectMapper mapper = new ObjectMapper();
-			InputStream in = new ClassPathResource(DATA).getInputStream();
-
-			events = mapper.readValue(in, mapper.getTypeFactory().constructCollectionType(List.class, Event.class));
-		} catch (Exception e) {
-			log.error("Exception while reading file '" + DATA + "': " + e);
-			// If we can't read the file, then the event list is empty...
-		}
-
-		return events;
+		return eventRepository.findAll();
+	}
+	
+	public Event save(Event e) {
+		return eventRepository.save(e);
 	}
 }
