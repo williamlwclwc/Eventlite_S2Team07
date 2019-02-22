54bb0a3e4a971c4b169705c2dc1174fb2ca46c31
Sterling Freeman <sterling.freeman@student.manchester.ac.uk>
2019-02-22 20:23:30 +0000

    Added feature to update event
    editEvent() to feed the event object to the update page.
    updateEvent() to save the updated event.

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -1,14 +1,22 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
 
 @Controller
 @RequestMapping(value = "/events", produces = { MediaType.TEXT_HTML_VALUE })
@@ -26,6 +34,33 @@ public class EventsController {
 		model.addAttribute("events", eventService.findAll());
 
 		return "events/index";
+	}
+
+	@RequestMapping(value = "update/{id}", method = RequestMethod.GET)
+	public String editEvent(@PathVariable("id") long id, Model model) {
+
+		Event e = eventService.findById(id);
+		model.addAttribute("event", e);
+		model.addAttribute("venues", venueService.findAll());
+
+		return "events/update";
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String updateEvent(@RequestBody @Valid @ModelAttribute Event event,
+			BindingResult errors, Model model, RedirectAttributes redirectAttrs) {
+		if (errors.hasErrors()) {
+			model.addAttribute("event", event);
+			model.addAttribute("venues", venueService.findAll());
+			return "events/update";
+		}
+
+		eventService.save(event);
+		redirectAttrs.addFlashAttribute("ok_message", "Event updated.");
+
+		return "redirect:/events";
+
 	}
 
 }
