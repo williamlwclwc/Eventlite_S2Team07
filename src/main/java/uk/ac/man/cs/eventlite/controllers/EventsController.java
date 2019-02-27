d1af20120c7ec76b585fa38ad47aab74f2b87e2c
Paul <paul.stefanescu@student.manchester.ac.uk>
2019-02-27 17:39:14 +0000

    Removed unnecessary imports

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -1,7 +1,5 @@
 package uk.ac.man.cs.eventlite.controllers;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
