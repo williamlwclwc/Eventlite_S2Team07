c3995c6981c569399599ccdbae3db9ba314acb57
Sterling Freeman <sterling.freeman@student.manchester.ac.uk>
2019-03-17 16:58:15 +0000

    Added Venue Test Cases. Still Needs to resolve "No ModelAndView" Error
    Implemented test cases for a valid update and various invalid update
    scenarios. Only issue seems to be an assertion error:
    "java.lang.AssertionError: No ModelAndView Found" Still needs fixing
    before merging with master

--- src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
@@ -1,6 +1,8 @@
 package uk.ac.man.cs.eventlite.controllers;
 
 import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
@@ -24,6 +26,7 @@ import javax.servlet.Filter;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -54,12 +57,12 @@ public class VenuesControllerTest {
 
 	private MockMvc mvc;
 	
-//	
-//	private final static String BAD_ROLE = "USER";
-//	private final static String name = "";
-//	private final static String roadName = "100 Oxford Road";
-//	private final static String postCode = "M33 13P";
-//	private final static int capacity = 100;
+	
+	private final static String BAD_ROLE = "USER";
+	private final static String name = "";
+	private final static String roadName = "100 Oxford Road";
+	private final static String postCode = "M33 13P";
+	private final static String capacity = "100";
 
 	@Autowired
 	private Filter springSecurityFilterChain;
@@ -107,54 +110,88 @@ public class VenuesControllerTest {
 		verifyZeroInteractions(venue);
 	}
 
-//	@Test
-//	public void getVenue() throws Exception {
-//		when(venueService.findById(1)).thenReturn(venue);
-//
-//		mvc.perform(MockMvcRequestBuilders.get("/venues/1").contentType(MediaType.TEXT_HTML)).andExpect(status().isOk())
-//		.andExpect(view().name("venues/1")).andExpect(handler().methodName("editVenue"));
-//
-//		verify(venue).getName();
-//	}
+	@Test
+	public void getVenue() throws Exception {
+		when(venueService.findById(1)).thenReturn(venue);
+
+		mvc.perform(MockMvcRequestBuilders.get("/venues/update/1").contentType(MediaType.TEXT_HTML)).andExpect(status().isOk())
+		.andExpect(view().name("venues/update")).andExpect(handler().methodName("editVenue"));
+
+		verify(venue);
+	}
 	
 	
 
-//	@Test
-//	public void updateVenueNoCsrf() throws Exception {
-//		mvc.perform(MockMvcRequestBuilders.post("/venues/1").contentType(MediaType.APPLICATION_FORM_URLENCODED)
-//				.param("name", name)
-//				.param("roadName", roadName)
-//				.param("postCode", postCode)
-//				.param("capacity", "100")
-//				.accept(MediaType.TEXT_HTML))
-//				.andExpect(status().isForbidden());
-//		
-//		verify(venueService, never()).save(venue);
-//		
-//	
-//	}
+	@Test
+	public void updateVenueNoCsrf() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.post("/venues/update/1").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("id", "1")
+				.param("name", name)
+				.param("roadName", roadName)
+				.param("postCode", postCode)
+				.param("capacity", "100")
+				.accept(MediaType.TEXT_HTML))
+				.andExpect(status().isForbidden());
+		
+		verify(venueService, never()).save(venue);
+		
 	
-//	@Test
-//	public void updateVenue() throws Exception {
-//		mvc.perform(MockMvcRequestBuilders.post("/venues/1").contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-//				.param("name", name)
-//				.param("roadName", roadName)
-//				.param("postCode", postCode)
-//				.param("capacity", "100")
-//				.param("id", "1")
-//				.accept(MediaType.TEXT_HTML).with(csrf()))
-//				.andExpect(status().isFound()).andExpect(content().string(""))
-//				.andExpect(view().name("redirect:/venues")).andExpect(model().hasNoErrors())
-//				.andExpect(handler().methodName("saveEditedVenue"));
-//		
-//		
-//		
-//	
-//	}
+	}
 	
+	@Test
+	public void updateVenue() throws Exception {
+		ArgumentCaptor<Venue> arg = ArgumentCaptor.forClass(Venue.class);
+		
+		mvc.perform(MockMvcRequestBuilders.post("/venues/update/1").contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+				.param("id", "1")
+				.param("name", name)
+				.param("roadName", roadName)
+				.param("postCode", postCode)
+				.param("capacity", capacity)
+				.accept(MediaType.TEXT_HTML).with(csrf()))
+				.andExpect(status().isFound()).andExpect(content().string(""))
+				.andExpect(view().name("redirect:/venues")).andExpect(model().hasNoErrors())
+				.andExpect(handler().methodName("saveEditedVenue"));
+		
+		verify(venueService).save(arg.capture());
+		assertThat(name, equalTo(arg.getValue().getName()));
 	
-
-
+	}
+	
+	@Test
+	public void UpdateWithoutName() throws Exception{
+		InvalidVenueUpdate("", roadName, postCode, "", "name");
+	}
+	
+	@Test
+	public void UpdateWithoutRoadName() throws Exception{
+		InvalidVenueUpdate(name, "", postCode, "", "roadName");
+	}
+	
+	@Test
+	public void UpdateWithoutPostCode() throws Exception{
+		InvalidVenueUpdate(name, roadName, "", capacity, "postCode");
+	}
+	
+	@Test
+	public void UpdateWithoutCapacity() throws Exception{
+		InvalidVenueUpdate(name, roadName, postCode, "", "capacity");
+	}
+	
+	private void InvalidVenueUpdate(String name1, String roadName1, String postCode1, String capacity1, String errors1) throws Exception{
+		mvc.perform(MockMvcRequestBuilders.post("/venues/update/1")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+				.param("id", "1")
+				.param("name", name1)
+				.param("roadName", roadName1)
+				.param("postCode", postCode1)
+				.param("capacity", capacity1)
+				.accept(MediaType.TEXT_HTML).with(csrf()))
+			.andExpect(status().isFound()).andExpect(view().name("venues/update/1"))
+			.andExpect(model().attributeHasFieldErrors("venue", errors1))
+			.andExpect(handler().methodName("saveEditedVenue")).andExpect(flash().attributeCount(0));
+		verify(venueService, never()).save(venue);
+	}
 
 }
 
