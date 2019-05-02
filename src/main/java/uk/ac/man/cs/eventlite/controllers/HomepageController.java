9a069f45c3ecf7d43db36f8aad4c9177057bc3cf
Paul Stefanescu <paul.stefanescu@student.manchester.ac.uk>
2019-05-02 12:43:47 +0100

    Added the final logic to display the most popular venues

--- src/main/java/uk/ac/man/cs/eventlite/controllers/HomepageController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/HomepageController.java
@@ -49,35 +49,50 @@ public class HomepageController {
 		while(itr.hasNext()) {
 			Event event = itr.next();
 			Venue venue = event.getVenue();
+			System.out.println("Event - " + event.getName() + " at venue - " + venue.getName());
 			if(!venues.contains(venue))
 			{
 				venues.add(venue);
 				numberOfEvents.add(1);
+				System.out.println("Added 1 for venue " + venue.getName());
 			}
 			else
 			{
 				numberOfEvents.set(venues.indexOf(venue), numberOfEvents.get(venues.indexOf(venue) + 1));
 			}
 		}
+		numberOfEvents.set(1, 2);
 		int max1 = -1, max2 = -1, max3 = -1;
+		int index1 = 0, index2 = 0, index3 = 0;
 		for(int i = 0; i < venues.size(); i++)
 		{
 			int currentNumber = numberOfEvents.get(i);
 			if( currentNumber > max1)
 			{
 				max3 = max2;
+				index3 = index2;
+				
 				max2 = max1;
+				index2 = index1;
+				
 				max1 = currentNumber;
+				index1 = i;
 			}
 			else
 				if(currentNumber > max2)
 				{
 					max3 = max2;
+					index3 = index2;
+					
 					max2 = currentNumber;
+					index2 = i;
 				}
 				else
 					if(currentNumber > max3)
+					{
 						max3 = currentNumber;
+						index3 = i;
+					}
 		}
 		
 		Iterator<Event> newItr = allEvents.iterator();
@@ -85,9 +100,9 @@ public class HomepageController {
 		model.addAttribute("event1", newItr.next());
 		model.addAttribute("event2", newItr.next());
 		model.addAttribute("event3", newItr.next());
-		model.addAttribute("venue1", venues.get(numberOfEvents.indexOf(max1)));
-		model.addAttribute("venue2", venues.get(numberOfEvents.indexOf(max2)));
-		model.addAttribute("venue3", venues.get(numberOfEvents.indexOf(max3)));
+		model.addAttribute("venue1", venues.get(index1));
+		model.addAttribute("venue2", venues.get(index2));
+		model.addAttribute("venue3", venues.get(index3));
 		
 		return "/index";
 	}
