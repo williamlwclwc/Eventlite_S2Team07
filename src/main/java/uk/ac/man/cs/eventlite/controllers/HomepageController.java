b9c6db363e42ea279354c3626938fadf2c6734ae
Paul Stefanescu <paul.stefanescu@student.manchester.ac.uk>
2019-05-05 15:29:19 +0100

    Removed unnecessary imports and variables from the homepage controller

--- src/main/java/uk/ac/man/cs/eventlite/controllers/HomepageController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/HomepageController.java
@@ -1,27 +1,16 @@
 package uk.ac.man.cs.eventlite.controllers;
 
-import javax.validation.Valid;
-
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
-import uk.ac.man.cs.eventlite.dao.VenueService;
 import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.entities.Venue;
 
@@ -32,8 +21,6 @@ public class HomepageController {
 	@Autowired
 	private EventService eventService;
 
-	@Autowired
-	private VenueService venueService;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String getAllEvents(Model model) {
