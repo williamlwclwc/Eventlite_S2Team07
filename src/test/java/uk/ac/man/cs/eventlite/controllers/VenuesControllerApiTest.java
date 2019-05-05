13d35b7038afbaf71a1f9ad4cd67c69356e61107
Paul Stefanescu <paul.stefanescu@student.manchester.ac.uk>
2019-05-05 16:02:24 +0100

    Added feature and integration tests for the Venue List REST responses

---
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApiTest.java
@@ -0,0 +1,88 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.empty;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.ac.man.cs.eventlite.testutil.MessageConverterUtil.getMessageConverters;
+
+import java.util.Collections;
+
+import javax.servlet.Filter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import uk.ac.man.cs.eventlite.EventLite;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Venue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EventLite.class)
+@AutoConfigureMockMvc
+@DirtiesContext
+@ActiveProfiles("test")
+public class VenuesControllerApiTest {
+
+	private MockMvc mvc;
+
+	@Autowired
+	private Filter springSecurityFilterChain;
+
+	@Mock
+	private VenueService eventService;
+
+	@InjectMocks
+	private VenuesControllerApi venuesController;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		mvc = MockMvcBuilders.standaloneSetup(venuesController).apply(springSecurity(springSecurityFilterChain))
+				.setMessageConverters(getMessageConverters()).build();
+	}
+
+	@Test
+	public void getIndexWhenNoVenues() throws Exception {
+		when(eventService.findAll()).thenReturn(Collections.<Venue> emptyList());
+
+		mvc.perform(get("/api/venues").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(handler().methodName("getAllVenues")).andExpect(jsonPath("$.length()", equalTo(1)))
+				.andExpect(jsonPath("$._links.self.href", endsWith("/api/venues")));
+
+		verify(eventService).findAll();
+	}
+
+	@Test
+	public void getIndexWithVenues() throws Exception {
+		Venue e = new Venue();
+		when(eventService.findAll()).thenReturn(Collections.<Venue> singletonList(e));
+
+		mvc.perform(get("/api/venues").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(handler().methodName("getAllVenues")).andExpect(jsonPath("$.length()", equalTo(2)))
+				.andExpect(jsonPath("$._links.self.href", endsWith("/api/venues")))
+				.andExpect(jsonPath("$._embedded.venues.length()", equalTo(1)));
+
+		verify(eventService).findAll();
+	}
+}
